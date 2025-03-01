f3d2a2e2c0211e08c481ea2375b1b59756e73bff
정필성 <ibetter.kr@gmail.com>
2025-03-01 16:59:49 +0900

    코드 수정

--- src/main/java/com/example/spring/posts/PostsController.java
+++ src/main/java/com/example/spring/posts/PostsController.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -16,25 +16,27 @@ public class PostsController {
     PostsService postsService;
 
     // 게시글 등록 (화면, GET)
-    @GetMapping("/posts/create")
+    @RequestMapping(value = "/posts/create", method = RequestMethod.GET)
     public String create() {
         return "posts/create";
     }
 
     // 게시글 등록 (처리, POST)
-    @PostMapping("/posts/create")
+    @RequestMapping(value = "/posts/create", method = RequestMethod.POST)
     public String createPost(PostsVo postsVo, RedirectAttributes redirectAttributes) {
         boolean created = postsService.create(postsVo);
+
         if (created) {
             redirectAttributes.addFlashAttribute("successMessage", "게시글이 등록되었습니다.");
             return "redirect:/posts/";
         }
+        
         redirectAttributes.addFlashAttribute("errorMessage", "게시글 등록에 실패했습니다.");
         return "redirect:/posts/create";
     }
 
-    // 게시글 목록
-    @GetMapping("/posts/")
+    // 게시글 목록 (화면, GET)
+    @RequestMapping(value = "/posts/", method = RequestMethod.GET)
     public String listGet(Model model) {
         List<PostsVo> postsVoList = postsService.list();
         model.addAttribute("postsVoList", postsVoList);
